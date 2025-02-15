b5c7a08891870a693869ed7bb0a1ff6043a39840
Ivlev Oleksandr <81967994+SanyaIvlev@users.noreply.github.com>
2025-02-14 19:43:24 -0600

    Small cleaning

--- Agar.io SFML/Engine/Audio/AudioSystem.cs
+++ Agar.io SFML/Engine/Audio/AudioSystem.cs
@@ -3,18 +3,11 @@ using SFML.Audio;
 
 namespace Agar.io_SFML.Audio;
 
-public class AudioSystem // розмiстив цей файл у папцi Game, бо тут enum AudioType Залежить вiд умов гри
+public class AudioSystem
 {
     private FileInfo[] _audioFilesInfo;
     
     private Dictionary<string, Sound> _sounds;
-
-    private string _firstKill;
-    private string _secondKill;
-    private string _thirdKill;
-    private string _fourthKill;
-    private string _fifthKill;
-    private string _kill;
 
     public AudioSystem()
     {
@@ -22,13 +15,6 @@ public class AudioSystem // розмiстив цей файл у папцi Game,
         
         DirectoryInfo directoryInfo = new DirectoryInfo(Path.GetFullPath(Directory.GetCurrentDirectory()));
         _audioFilesInfo = directoryInfo.GetFiles("*.mp3");
-
-        _firstKill = AudioConfig.FirstBlood;
-        _secondKill = AudioConfig.DoubleKill;
-        _thirdKill = AudioConfig.TripleKill;
-        _fourthKill = AudioConfig.UltraKill;
-        _fifthKill = AudioConfig.MegaKill;
-        _kill = AudioConfig.Kill;
     }
 
     public void Initialize()
@@ -47,28 +33,5 @@ public class AudioSystem // розмiстив цей файл у папцi Game,
     public void PlaySoundOnce(string audioName)
     {
         _sounds[audioName].Play();
-    }
-
-    public void PlaySoundLooped(string audioName)
-    {
-        var sound = _sounds[audioName];
-        
-        sound.Loop = true;
-        sound.Play();
-    }
-    
-    public void PlayEliminationSound(int eliminationsNumber)
-    {
-        string playingSoundType = eliminationsNumber switch
-        {
-            1 => _firstKill,
-            2 => _secondKill,
-            3 => _thirdKill,
-            4 => _fourthKill,
-            5 => _fifthKill,
-            _ => _kill,
-        };
-        
-        PlaySoundOnce(playingSoundType);
     }
 }
